06feead1d5d86489b4605b0a1666a7eb5b0b192b
fanyalvarez <alvarezpestefania@outlook.com>
2023-08-30 22:26:37 -0600

    add until No 79

--- 04-gif-app-vite/src/components/AddCategory.jsx
+++ 04-gif-app-vite/src/components/AddCategory.jsx
@@ -2,16 +2,29 @@ import { useState } from "react";
 
 export const AddCategory = () => {
   const [inputValue, setInputValue] = useState("avatar");
-  const onChangeInt = () => {
-    setInputValue("hello");
+
+  // const onChangeInt = (event) => {
+  const onChangeInt = ({ target }) => {//desestructuracion del event
+    // setInputValue("hello");
+    // console.log(event.target.value);      //console.log
+    // setInputValue(event.target.value)
+    setInputValue(target.value); //desestructuracion del event
   };
 
+  const onSubmit=(event)=>{
+    event.preventDefault()
+    console.log(inputValue);
+  }
+
   return (
-    <input
-      type="text"
-      placeholder="Buscar gifs"
-      value={inputValue}
-      onChange={(event) => onChangeInt(event)}
-    />
+    <form onSubmit={(event)=>onSubmit(event)}>
+      <input
+        type="text"
+        placeholder="Buscar gifs"
+        value={inputValue}
+        // onChange={(event) => onChangeInt(event)}
+        onChange={onChangeInt} //simplificado
+      />
+    </form>
   );
 };
